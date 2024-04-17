1ae0f0f83292a2f657acc5ef01c03f1e32a07a55
FaceNach <ignacioijg@gmail.com>
2024-04-17 09:37:39 -0300

    First commit of the project

---
+++ BulkyWebRazon _Temp/Pages/Categories/Create.cshtml.cs
@@ -0,0 +1,30 @@
+using BulkyWebRazon__Temp.Data;
+using BulkyWebRazon__Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BulkyWebRazon__Temp.Pages.Categories
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext _DB;
+        [BindProperty]
+        public Category Category { get; set; }
+        public CreateModel(ApplicationDbContext DB)
+        {
+            _DB = DB;
+        }
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {                       
+			_DB.Categories.Add(Category);
+		    _DB.SaveChanges();
+            TempData["success"] = "Category created succesfully";
+
+            return RedirectToPage("Index");
+        }
+    }
+}
