5d88e6391db96d22e5fe54c0e8a42f0e6bff1ed8
FaceNach <ignacioijg@gmail.com>
2024-04-24 10:42:19 -0300

    Added home page and details page with functionality

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,3 +1,5 @@
+using Bulky.DataAccess.Repository;
+using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
@@ -8,18 +10,27 @@ namespace BulkyWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _UnitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork UnitOfWork)
         {
             _logger = logger;
+            _UnitOfWork = UnitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> ProductList = _UnitOfWork.Product.GetAll(IncludeProperties:"Category");
+            return View(ProductList);
         }
 
-        public IActionResult Privacy()
+		public IActionResult Details(int? id)
+		{
+			Product ProductDetails = _UnitOfWork.Product.Get(u => u.Id == id, IncludeProperties: "Category");
+			return View(ProductDetails);
+		}
+
+		public IActionResult Privacy()
         {
             return View();
         }
